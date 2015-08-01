d287e78cbb7531ccc40d02bbe66cd700bbcda528
Benjamin Kniffler <bkniffler@me.com>
2015-08-01 12:42:30 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # express-sequelize-user
 Expose sequelize query functions in express requests that inject the current user in returned objects
+
+## Install
+```shell
+npm install express-sequelize-user --save
+```
+
+## Usage
+```javascript
+// Bind middlewares
+...
+app.use(passport.initialize());
+app.use(passport.session());
+// Bind express-sequelize-user
+require("express-sequelize-user")(app);
+...
+
+var User = sequelize.define("User", {});
+
+// Expose put user
+app.put("/api/user/:id", app.isAuthenticated, function (req, res, next) {
+   // Expose put user
+   req.context.findById(User, req.params.id).then(function(item){
+      // item.context.user is now req.user
+      if(!user){
+         return next(new Error("User with id " + id + " not found"));
+      }
+      user.updateAttributes(req.body).then(function(user) {
+         res.json(user);
+      }).catch(next);
+   }).catch(next);
+});
+
+// Add a hook
+User.addHook("afterUpdate", function(instance){
+   if(instance.context && instance.context.user){
+      console.log("A user was changed by " + instance.context.user.id);
+   }
+});
+```
---
+++ index.js
@@ -0,0 +1,62 @@
+// Return express middleware that exposes sequelize query equivalents in req.context with user injected in
+// object instances
+//
+// e.g. req.context.findById(User, 1) => {dataValues: {id: 1}, context}
+//
+// requires req.user to be available
+module.exports = function(app, options){
+   app.use(function(req, res, next){
+      if(!req.context){
+         req.context = {};
+      }
+      var arrays = ["findAll"];
+      var singles = ["findById", "findOne", "findOrInitialize", "findOrCreate", "create"];
+      var singleNoPromise = ["build"];
+
+      // Make req.context.findAll and inject user to results
+      arrays.forEach(function(name){
+         req.context[name] = function(model){
+            // Strip model from arguments
+            Array.prototype.shift.apply(arguments);
+            // Apply original function
+            return model[name].apply(model, arguments).then(function(results){
+               results.forEach(function(result){
+                  result.context = {
+                     user: req.user
+                  }
+               })
+               return results;
+            });
+         };
+      });
+      // Make req.context.findById, findOne, ... and inject user to result
+      singles.forEach(function(name){
+         req.context[name] = function(model){
+            // Strip model from arguments
+            Array.prototype.shift.apply(arguments);
+            // Apply original function
+            return model[name].apply(model, arguments).then(function(result){
+               console.log(result);
+               result.context = {
+                  user: req.user
+               }
+               return result;
+            });
+         };
+      });
+      // Make req.context.build and inject user to result
+      singleNoPromise.forEach(function(name){
+         req.context[name] = function(model){
+            // Strip model from arguments
+            Array.prototype.shift.apply(arguments);
+            // Apply original function
+            var result = model[name].apply(model, arguments);
+            result.context = {
+               user: req.user
+            }
+            return result;
+         };
+      });
+      next();
+   });
+}
